9c6b88bcf60f78dab814e86250261918babe112c
edwardzhong <edwardloveyou@gmail.com>
2019-10-24 11:26:08 +0800

    webpack prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,5 +3,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  output: {
+    publicPath: './',//使用相对路径(./)生成可以静态访问的页面，绝对路径(/)才能devServer使用
+  },
 });
