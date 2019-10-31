7a1254d93b6ecc05d66954642ae4ccd07922a6bb
edwardzhong <edwardloveyou@gmail.com>
2019-10-31 09:08:17 +0800

    req

--- src/common/request.js
+++ src/common/request.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import config from '../config/app'
 
 axios.defaults.baseURL = config.url;
+axios.defaults.timeout = 20000;
+axios.defaults.withCredentials = true;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 
 axios.interceptors.request.use(function(config) {
@@ -9,8 +11,11 @@ axios.interceptors.request.use(function(config) {
     const params = config.method == "get" ? config.params || {} : config.data || {};
     if (info) {
         const obj = JSON.parse(info);
-        if (!params.userId && obj && obj.id) {
-            Object.assign(params, { userId: obj.id });
+        // if (!params.userId && obj && obj.id) {
+        //     Object.assign(params, { userId: obj.id });
+        // }
+        if(obj.token){
+            config.headers.Authorization = 'Bearer ' + obj.token;
         }
     }
     params._t = Math.random();
@@ -29,5 +34,7 @@ axios.interceptors.response.use(function(res) {
     return res.data;
 });
 
+export const get = (url, param) => axios.get(url, { params: param });
 export const post = (url, param) => axios.post(url, param);
-export const get = (url, param) => axios.get(url, param);
+export const put = (url, param) => axios.put(url, param);
+export const del = (url, param) => axios.delete(url, {data: param});
