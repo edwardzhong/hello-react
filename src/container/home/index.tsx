71b027d5421c46a6d02961554fbb8d7d15bef4ed
edwardzhong <edwardloveyou@gmail.com>
2019-11-07 15:43:42 +0800

    update

--- src/container/home/index.tsx
+++ src/container/home/index.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { getContext } from '@/context'
 import './style.scss'
 
-const Home = ({history}) => {
+const Home = () => {
     const { state, action } = getContext();
     const { user } = state;
     const { setUser } = action;
