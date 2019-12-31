2ff860e724d9654d62cd5f82abc5cdd331197c04
edwardzhong <edwardloveyou@gmail.com>
2019-12-31 19:36:42 +0800

    req

--- src/common/request.ts
+++ src/common/request.ts
@@ -35,6 +35,18 @@ axios.interceptors.response.use(res => {
     return res.data;
 });
 
+export const form: AxiosFn = (url, data) => {
+    return axios({
+        headers: { 'Content-Type': 'multipart/form-data;chartset=UTF-8' },
+        method: 'post',
+        url,
+        data
+    }).then(res => {
+        return res.data;
+    }, (err: Error) => {
+        return { code: -99, msg: err.message };
+    });
+}
 export const get: AxiosFn = (url, param) => {
     return axios.get(url, { params: param }).then(res => {
         return res.data;
