801779e5004c5965fe2a7e54a1afacf1fbc5f441
edwardzhong <zhongjfgd@163.com>
2020-02-03 22:18:09 +0800

    tab

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,13 +2,13 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        port: 4001,
-        compress: true,
-        historyApiFallback: true,
-        hot: true
-    }
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+		port: 4001,
+		compress: true,
+		historyApiFallback: true,
+		hot: true,
+	},
 });
